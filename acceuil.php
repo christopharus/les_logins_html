d03abc6cc78e8b3502c72a0dd773928273ba54c0
christopharus <armelzong@gmail.com>
2019-03-19 09:43:18 -0600

    premiere page

---
+++ acceuil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+	<html>
+		
+		<head>	
+			<meta charset="utf-8" />
+			<title>Les pintades</title>
+		</head>	
+
+		<body>		
+			<header>
+			
+			<header>
+
+
+			<section>
+		
+
+			</section>
+
+
+			<footer>
+
+			</footer>
+
+
+		</body>
+
+	</html>
